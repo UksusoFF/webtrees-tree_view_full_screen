fdf42636e4d29fc01a23ff38b1db8dccdf098b0c
Kirill Uksusov <uksusoff@yandex.ru>
2020-06-24 16:57:51 +0400

    Fixed work on webtrees 2.0.6

--- resources/scripts/module.js
+++ resources/scripts/module.js
@@ -1,14 +1,17 @@
 TreeViewHandler.prototype.installFullScreen = function() {
     var self = this;
     var $treeTools = self.toolbox.find('ul');
-    var $button = $('<li id="tvfs" class="tv_button tvfs-switch-full-screen" title="Toggle FullScreen View"></li>');
 
-    $button.on('click', function(e) {
-        self.treeview.parent().toggleClass('tvfs-full-screen');
-        self.treeview.closest('.wt-ajax-load').toggleClass('tvfs-full-screen');
-    });
+    if (!$treeTools.find('li#tvfs').length) {
+        var $button = $('<li id="tvfs" class="tv_button tvfs-switch-full-screen" title="Toggle FullScreen View"></li>');
 
-    $treeTools.append($button);
+        $button.on('click', function(e) {
+            self.treeview.parent().toggleClass('tvfs-full-screen');
+            self.treeview.closest('.wt-ajax-load').toggleClass('tvfs-full-screen');
+        });
+
+        $treeTools.append($button);
+    }
 };
 
 TreeViewHandler.prototype.setCompleteOriginal = TreeViewHandler.prototype.setComplete;
@@ -21,5 +24,8 @@ TreeViewHandler.prototype.setComplete = function() {
 $(document).ready(function() {
     if (typeof tvHandler != 'undefined') {
         tvHandler.installFullScreen();
+    }
+    if (typeof tvTabHandler != 'undefined') {
+        tvTabHandler.installFullScreen();
     }
 });
